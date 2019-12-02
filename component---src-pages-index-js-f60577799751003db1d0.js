78fef7b3f87d4d4938997380aed80c65fa546d5b
Kazuto Kusama <jacopen@gmail.com>
2019-12-02 22:45:16 +0900

    Updates

---
+++ component---src-pages-index-js-f60577799751003db1d0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,t,n){"use strict";n.r(t);var l=n("q1tI"),a=n.n(l);t.default=function(){return a.a.createElement("div",{style:{color:"purple"}},a.a.createElement("h1",null,"Hello Gatsby!"),a.a.createElement("p",null,"What a world."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-f60577799751003db1d0.js.map
