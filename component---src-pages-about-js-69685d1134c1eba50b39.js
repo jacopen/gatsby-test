e6ca2296419be6a0d59d2126d3e29e82f6c670e8
Kazuto Kusama <jacopen@gmail.com>
2019-12-03 00:07:09 +0900

    Updates

---
+++ component---src-pages-about-js-69685d1134c1eba50b39.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=function(e){return r.a.createElement("h1",null,e.headerText)};t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-69685d1134c1eba50b39.js.map
